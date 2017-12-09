e8a9903f86d725dfb6a6fc7d239b54fa24547c44
Unknown <michael.villaronga@gmail.com>
2017-12-09 15:54:56 -0600

    Layout of Login box

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import AppBar  from 'react-toolbox/lib/app_bar/AppBar';
 import Navigation from 'react-toolbox/lib/navigation/Navigation';
+import { Grid, Row, Col } from 'react-material-responsive-grid';
 
 import './toolbox/theme.css';
 import theme from './toolbox/theme';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 
+import Logon from './logon/Logon';
+
 class App extends Component {
+
+  centerSizes = ['xs','sm','md','lg','xl'];
+
   render() {
     return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <div>
           <AppBar title='Chore Tracker' leftIcon='menu' >
             <Navigation />
           </AppBar>
-        </div>
+          <Grid fixed={'center'}>
+            <Row>
+              <Col><br /></Col>
+            </Row>
+            <Row center={this.centerSizes} >
+                <Col>
+                  <Logon />
+                </Col>
+            </Row>
+          </Grid>
+          
       </div>
     </ThemeProvider>
     );
